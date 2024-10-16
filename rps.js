d6c122b23335795d574953fda8731bf8b29bd1ad
Pooja Gurung <pooja.gurung@ada.ac.uk>
2024-10-16 09:59:35 +0000

    Draw condition works.

--- rps.js
+++ rps.js
@@ -1,16 +1,21 @@
 function rockPaperScissors(player1, player2) {
-
-  
+  // keep data type strict so use === for comparision
+  if (player1 === player2){
+    return "draw"
+  }
    
 }
 
 
 
 
+// #below my personal testing
+// $node rps.js
+console.log(rockPaperScissors("rock", "rock"))
+console.log(rockPaperScissors("rock", "scissors"))
 
-
-
-
+// $npm run test - to run unit test in the terminal
+// 6 areas of test --includes addional area/criteria
 
 
 // Leave this code here for the automated tests
