51794408b0d8c0e3ace9bccab08339019137a12f
Pooja Gurung <pooja.gurung@ada.ac.uk>
2024-10-16 15:32:36 +0000

    all test passed. additonal criteria met.

--- rps.js
+++ rps.js
@@ -9,51 +9,32 @@ function rockPaperScissors(player1, player2) {
   } else if (
     (player1.toLowerCase() === "rock" && player2.toLowerCase() === "scissors") || 
     (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "paper") ||
-    (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock")
+    (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock") ||
+    // additional criteria
+    (player1.toLowerCase() === "lizard" && player2.toLowerCase() === "spock") ||
+    (player1.toLowerCase() === "spock" && player2.toLowerCase() === "scissors") ||
+    (player1.toLowerCase() === "rock" && player2.toLowerCase() === "lizard") ||
+    (player1.toLowerCase() === "paper" && player2.toLowerCase() === "spock") ||
+    (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "lizard") ||
+    (player1.toLowerCase() === "lizard" && player2.toLowerCase() === "paper") ||
+    (player1.toLowerCase() === "spock" && player2.toLowerCase() === "rock") 
   ){
     return `${player1Winner}`
   } else if (
     (player2.toLowerCase() === "rock" && player1.toLowerCase() === "scissors") ||
     (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "paper") ||
-    (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock")
+    (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock") ||
+    // additional criteria
+    (player2.toLowerCase() === "lizard" && player1.toLowerCase() === "spock") ||
+    (player2.toLowerCase() === "spock" && player1.toLowerCase() === "scissors") ||
+    (player2.toLowerCase() === "rock" && player1.toLowerCase() === "lizard") ||
+    (player2.toLowerCase() === "paper" && player1.toLowerCase() === "spock") ||
+    (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "lizard") ||
+    (player2.toLowerCase() === "lizard" && player1.toLowerCase() === "paper") ||
+    (player2.toLowerCase() === "spock" && player1.toLowerCase() === "rock") 
   ){
     return `${player2Winner}`
   }
-  // else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "scissors"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "scissors"){
-  //   return "player2"
-  // } else if (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "paper"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "paper"){
-  //   return "player2"
-
-  // } else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock"){
-  //   return "player2"
-
-  // // ---------!Additional criteria!-------
-  // } else if (player1.toLowerCase() === "lizard" && player2.toLowerCase() === "spock"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "lizard" && player1.toLowerCase() === "spock"){
-  //   return "player2"
-  // } 
-  // else if (player1.toLowerCase() === "spock" && player2.toLowerCase() === "scissors"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "spock" && player1.toLowerCase() === "scissors"){
-  //   return "player2"
-  // } 
-  // else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "lizard"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "lizard"){
-  //   return "player2"
-  // } 
-  // else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "spock"){
-  //   return "player1"
-  // } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "spock"){
-  //   return "player2"
-  // } 
 }
 // #below my personal testing
 // $node rps.js
@@ -67,13 +48,28 @@ console.log("-----2 scissor>paper------")
 console.log(rockPaperScissors("Paper", "Rock"))
 console.log(rockPaperScissors("Rock", "Paper"))
 console.log("-----3 paper>rock------")
-// console.log(rockPaperScissors("lizard", "spock"))
-// console.log(rockPaperScissors("spock", "lizard"))
-// console.log("------4 lizzard>spock-----")
-// console.log(rockPaperScissors("rock", "lizard"))
-// console.log(rockPaperScissors("lizard", "rock"))
-// console.log("------4 rock>lizzard-----")
-
+console.log("-----additional criteria------")
+console.log(rockPaperScissors("lizard", "spock"))
+console.log(rockPaperScissors("spock", "lizard"))
+console.log("------4 lizzard>spock-----")
+console.log(rockPaperScissors("spock", "scissors"))
+console.log(rockPaperScissors("scissors", "spock"))
+console.log("------5 spock>scissors-----")
+console.log(rockPaperScissors("rock", "lizard"))
+console.log(rockPaperScissors("lizard", "rock"))
+console.log("------6 rock>lizzard-----")
+console.log(rockPaperScissors("paper", "spock"))
+console.log(rockPaperScissors("spock", "paper"))
+console.log("------7 paper>spock-----")
+console.log(rockPaperScissors("scissors", "lizard"))
+console.log(rockPaperScissors("lizard", "scissors"))
+console.log("------8 scissors>lizard-----")
+console.log(rockPaperScissors("lizard", "paper"))
+console.log(rockPaperScissors("paper", "lizard"))
+console.log("------9 lizard>paper-----")
+console.log(rockPaperScissors("spock", "rock"))
+console.log(rockPaperScissors("rock", "spock"))
+console.log("------10 spock>rock-----")
 
 
 // $npm run test - to run unit test in the terminal
