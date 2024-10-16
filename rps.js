4842dcd0d2d5aa53da182eff652d9a46f7565bbe
Pooja Gurung <pooja.gurung@ada.ac.uk>
2024-10-16 15:16:44 +0000

    refactoring the if..else for basic critera

--- rps.js
+++ rps.js
@@ -1,39 +1,59 @@
 function rockPaperScissors(player1, player2) {
+  
+  
+  let player1Winner = "player1";
+  let player2Winner = "player2";
+
   if (player1 === player2){
-    return "draw"
-  } else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "scissors"){
-    return "player1"
-  } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "scissors"){
-    return "player2"
-  } else if (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "paper"){
-    return "player1"
-  } else if (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "paper"){
-    return "player2"
-  } else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock"){
-    return "player1"
-  } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock"){
-    return "player2"
-  // ---------!Additional criteria!-------
-  } else if (player1.toLowerCase() === "lizard" && player2.toLowerCase() === "spock"){
-    return "player1"
-  } else if (player2.toLowerCase() === "lizard" && player1.toLowerCase() === "spock"){
-    return "player2"
-  } 
-  else if (player1.toLowerCase() === "spock" && player2.toLowerCase() === "scissors"){
-    return "player1"
-  } else if (player2.toLowerCase() === "spock" && player1.toLowerCase() === "scissors"){
-    return "player2"
-  } 
-  else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "lizard"){
-    return "player1"
-  } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "lizard"){
-    return "player2"
-  } 
-  else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "spock"){
-    return "player1"
-  } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "spock"){
-    return "player2"
-  } 
+    return "draw" 
+  } else if (
+    (player1.toLowerCase() === "rock" && player2.toLowerCase() === "scissors") || 
+    (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "paper") ||
+    (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock")
+  ){
+    return `${player1Winner}`
+  } else if (
+    (player2.toLowerCase() === "rock" && player1.toLowerCase() === "scissors") ||
+    (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "paper") ||
+    (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock")
+  ){
+    return `${player2Winner}`
+  }
+  // else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "scissors"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "scissors"){
+  //   return "player2"
+  // } else if (player1.toLowerCase() === "scissors" && player2.toLowerCase() === "paper"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "scissors" && player1.toLowerCase() === "paper"){
+  //   return "player2"
+
+  // } else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "rock"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "rock"){
+  //   return "player2"
+
+  // // ---------!Additional criteria!-------
+  // } else if (player1.toLowerCase() === "lizard" && player2.toLowerCase() === "spock"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "lizard" && player1.toLowerCase() === "spock"){
+  //   return "player2"
+  // } 
+  // else if (player1.toLowerCase() === "spock" && player2.toLowerCase() === "scissors"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "spock" && player1.toLowerCase() === "scissors"){
+  //   return "player2"
+  // } 
+  // else if (player1.toLowerCase() === "rock" && player2.toLowerCase() === "lizard"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "rock" && player1.toLowerCase() === "lizard"){
+  //   return "player2"
+  // } 
+  // else if (player1.toLowerCase() === "paper" && player2.toLowerCase() === "spock"){
+  //   return "player1"
+  // } else if (player2.toLowerCase() === "paper" && player1.toLowerCase() === "spock"){
+  //   return "player2"
+  // } 
 }
 // #below my personal testing
 // $node rps.js
@@ -47,12 +67,12 @@ console.log("-----2 scissor>paper------")
 console.log(rockPaperScissors("Paper", "Rock"))
 console.log(rockPaperScissors("Rock", "Paper"))
 console.log("-----3 paper>rock------")
-console.log(rockPaperScissors("lizard", "spock"))
-console.log(rockPaperScissors("spock", "lizard"))
-console.log("------4 lizzard>spock-----")
-console.log(rockPaperScissors("rock", "lizard"))
-console.log(rockPaperScissors("lizard", "rock"))
-console.log("------4 rock>lizzard-----")
+// console.log(rockPaperScissors("lizard", "spock"))
+// console.log(rockPaperScissors("spock", "lizard"))
+// console.log("------4 lizzard>spock-----")
+// console.log(rockPaperScissors("rock", "lizard"))
+// console.log(rockPaperScissors("lizard", "rock"))
+// console.log("------4 rock>lizzard-----")
 
 
 
