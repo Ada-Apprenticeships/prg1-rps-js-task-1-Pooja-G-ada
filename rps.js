0ecb11db32cb91bbf19a59caaa8bc82a8e793944
Pooja Gurung <pooja.gurung@ada.ac.uk>
2024-10-18 14:25:06 +0000

    removed my personal tests (console.logs) before submission. added return where missed

--- rps.js
+++ rps.js
@@ -12,9 +12,9 @@ function rockPaperScissors(player1, player2) {
   let player2Winner = "player2";
   
   // check whether its a draw or determine the winner
-  if (player1 === player2) "draw"
+  if (player1 === player2) return "draw"
   else {
-    (winConditions[player1.toLowerCase()].includes(player2.toLowerCase())) ? player1Winner : player2Winner;
+    return (winConditions[player1.toLowerCase()].includes(player2.toLowerCase())) ? player1Winner : player2Winner;
   }
 }
 
